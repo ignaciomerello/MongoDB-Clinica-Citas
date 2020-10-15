babdae146c90fc486337df7347590d4787a97bd7
Ignacio Merello Lloret <ignaciomerellolloret@gmail.com>
2020-10-15 14:03:10 +0200

    Create Model & Controller Users

---
+++ db/dbuser.js
@@ -0,0 +1,18 @@
+const UserModel = require('../models/User');
+
+const mongoose = require('mongoose');
+
+const fs = require('fs');
+
+const showUsers = (req, res) => {
+    UserModel.find({})
+        .then(users => {
+            res.send(users)
+        })
+        .catch(error => (error));
+}
+
+
+module.exports = {
+    showUsers
+};
---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+
+    username:{
+        type:String,
+        required:true //Indisplensable
+    },
+
+
+    email:{
+        unique:true,
+        type:String,
+        required:true //Indisplensable sequelize "AllowNull = false;"
+    },
+
+    password:{
+        type:String,
+        required:true //Indisplensable sequelize "AllowNull = false;"
+    },
+
+
+});
+
+const UserModel = mongoose.model('user', UserSchema);
+
+module.exports = UserModel;
